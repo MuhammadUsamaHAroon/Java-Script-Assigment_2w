ea7cbf8b21bafcfed944ec9ec1fe9f6de4fc669e
usama haroon <muhammadusamaharoon13579@gmail.com>
2020-11-01 18:47:29 +0500

    initialize

---
+++ index.js
@@ -0,0 +1,184 @@
+// var myage=18
+// var yourage= "19"
+// var sum=myageyourage
+// alert(sum)
+// var englishMarks=56
+// var physicsMarks=86
+// var mathMarks=70
+// var urduMarks=62
+// var islamiyatMarks=70
+
+// var obtainmarks=englishMarks+physicsMarks+mathMarks+urduMarks+islamiyatMarks
+
+//  percentage=obtainmarks/500*100
+
+// alert("marks of English is: "+englishMarks)
+// alert("marks of Physicks is: "+physicsMarks)
+// alert("marks of Methamatis is: "+mathMarks)
+// alert("marks of Urdu is: "+urduMarks)
+// alert("marks of Islamiyat is: "+islamiyatMarks)
+// alert ("total marks: "+obtainmarks)
+// alert("percentage is: "+percentage+"%")
+
+// // Calculate the batting average of Shahid Afridi in 4 innigs
+
+//  80
+//  55
+//  100
+//  45
+// var average=80+55+100+45
+// var totalaverage=average/4
+// alert ("Baber Azam batting average in 4 innings is: "+totalaverage)
+
+
+// var name="Usama"
+// alert(name)
+// var name=100
+// alert(name)
+
+// var first_name="Muhammad "
+// var middle_name="Usama "
+// var last_name="Haroon "
+// var completename=first_name+middle_name+last_name
+// alert(first_name)
+// alert(middle_name)
+// alert(last_name)
+
+// alert("Full Name of Student is : "+first_name+middle_name+last_name)
+
+// var m=150
+// var c=10
+// var e=m*c^2
+
+// alert("E=m*c^2= "+e)
+
+
+// var num1= 30
+// var  num3= 10
+// var sum= num1+num3
+
+// console.log('sum of these two numbers are '+ sum)
+
+// var item1=+prompt("Enter your item 1 price:")
+// var quantity1=+prompt("Enter your Quantity of iitem 1: ")
+// var item2=+prompt("Enter your item 2 price: ")
+// var quantity2=+prompt("Enter your quantity of item 2: ")
+// var shippingcharges=+prompt("The Shipping charges is: ")
+// var answer1=item1*quantity1
+// var answer2=item2*quantity2
+// var totalanswer=answer1+answer2+shippingcharges
+// console.log("The Price of item 1 is: "+item1)
+// console.log("Quantity of item 1 is: "+quantity1)
+// console.log("The Price of item two is: "+item2)
+// console.log("Quantity of item 2 is: "+quantity2)
+// console.log("The Shipping charges is: "+shippingcharges)
+// console.log("Total Price of item 1: "+answer1)
+// console.log("The total Price of item 2: "+answer2)
+// console.log("The total amount of item 1 and item 2 is: "+totalanswer)
+
+// var color=prompt("which color: ")
+
+// if(color==="red")
+// {alert ("Vichles must stop") 
+// }
+// else if
+//         (color==="yellow"){
+// alert("Vichles should get ready to move ")
+// }
+// else if
+//     (color==="green"){
+//     alert("Vichles must go now")
+// }
+// else{
+//  alert("wrong color")}
+
+// var english=+prompt("English marks:")
+// var math=+prompt("Math marks:")
+// var urdu=+prompt("Urdu marks:")
+// var physics=+prompt("Physics marks:")
+// var cHemistry=+prompt("Chemistry marks:")
+// var obtmark=english+math+urdu+cHemistry+physics
+// var totalmark=500
+// var percentage=obtmark*100/totalmark
+// console.log("Obtain marks is: "+obtmark)
+// console.log("Total marks is: "+totalmark)
+// console.log("Percentage: "+percentage+"%")
+// if(percentage>=80){console.log("Grade A one ") 
+// console.log("Remarks: "+"Excellent")
+// }
+// else if(percentage>=70){console.log("Grade A")
+// console.log("Remarks: "+"Good")
+// }
+// else if (percentage>=60){console.log("Grade B")
+// console.log("Remarks: "+"You need to improve") 
+// }
+// else{console.log("Fail")
+// console.log("Remarks: "+"Sorry")
+// }
+
+// 1>num2 && num3){
+
+// alert(num1+"First number is greater than all numbers")
+// }
+// else if(num2>num1 && num3){
+// alert(num2+"Second number is greater then all number")
+// }
+// else if(num3>num2 && num1){
+//     alert(num3+"third number is greater than all number")
+// }
+// var num1=+prompt("Enter the first number")
+// var num2=+prompt("Enter the seconnd number")
+// var num3=+prompt("Enter the third number")
+// if(num
+// var qualification=prompt("Your Qualification")
+// var year=+prompt("Your years of Service")
+// var salary=+prompt("Your current Salary")
+// var bsbonus=10000
+// var totalsalaryofbs=salary+bsbonus
+// var msbonus=15000
+// var totalsalaryofms=salary+msbonus
+// var msbonus1=20000
+// var totalsalaryofms1=salary+msbonus1
+
+// if(qualification==="Bachelors" && year<=10){
+//     alert("Your Current Salary with Bonus is "+totalsalaryofbs+"K")
+// }
+
+// else if(qualification==="Masters" && year>10){
+//     alert("Your Current Salary with Bonus is "+totalsalaryofms+"K")
+// }
+// else if(qualification==="Masters" && salary>=20000){
+//     alert("Your Current Salary with Bonus is "+totalsalaryofms1+"K")
+// }
+
+// var arr=["Aalo", "Piyaz", "Timater"]
+// console.log(arr)
+// arr.push("Laoki")
+// console.log(arr)
+// arr.pop("")
+
+// var pet=[]
+// console.log(pet)
+// pet[0]="Cat"
+// pet[1]="Dog"
+// console.log(pet)
+ var email=prompt("Enter the E-mail")
+ console.log(email)
+ var newemail=email.toLowerCase()
+ console.log(newemail)
+
+//  var password=prompt("Enter your Password")
+//  var newpassword=password.toLowerCase()
+
+
+// console.log(change)
+
+
+//  var password=prompt("Enter your Password")
+//  var changepassword=password.toLowerCase
+ 
+var array=[]
+array[0]="Bangladesh"
+array[1]="India"
+array[6]="china"
+console.log(array)
