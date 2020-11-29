72b05a45a00533beab03302efa36e8a27856a6d1
usama haroon <muhammadusamaharoon13579@gmail.com>
2020-11-29 16:43:03 +0500

    Cricket Math

--- index.js
+++ index.js
@@ -298,48 +298,92 @@
 
 // }
 
-var team1=prompt('Enter your 1st Team Name')
-var team2=prompt('Enter your 2nd Team Name')
 
-var numberofover=+prompt('Enter the number of Overs')
 
-var toss=Math.random ()*10
-var roundetoss=Math.round(toss)
-console.log(roundetoss)
 
-if(roundetoss<5){
-  alert(team1+'have won the toss &  elected to bat first')
-}
-else{
-  alert(team2+'have won the toss & elected to bat first')
-}
 
-var score=0
-var overs=0
-var wickets=0
+// var team1=prompt('Enter your 1st Team Name')
+// var team2=prompt('Enter your 2nd Team Name')
 
-alert('Lets Play')
-for(var i=1;i<=numberofover;i++){
-  for(var j=1;j<=6;j++){
-    var bowl=Math.random()*7
-    var newbowl=Math.round(bowl)
-    Math.round(bowl)
-if(newbowl===0){
-  wickets=wickets+1
-  alert('HOWZAT!!! thats a wicket ')
-}
+// var numberofover=+prompt('Enter the number of Overs')
 
-else if(newbowl===1 || newbowl===2 || newbowl===3 || newbowl===4 || newbowl===5 || newbowl===6){
-  score=score+newbowl
-alert(newbowl+'runs scored')
-}
-else if(newbowl===4 || newbowl===6){
-  alert('WOWW!!thats a Boundry')
-}
-}
-over=overs+1
-var wickets=0
-alert('The total Score at the end of '+1+'Overs'+'is'+score+'/'+wickets)
-  };
+// var toss=Math.random ()*10
+// var roundetoss=Math.round(toss)
+// console.log(roundetoss)
 
-alert()
+// if(roundetoss<5){
+//   alert(team1+' have won the toss &  elected to bat first')
+// }
+// else{
+//   alert(team2+' have won the toss & elected to bat first')
+// }
+
+// var score1=0
+// var overs=0
+// var wickets1=0
+
+// alert('Lets Play')
+// for(var i=1;i<=numberofover;i++){
+//   for(var j=1;j<=6;j++){
+//     var bowl=Math.random()*7
+//     var newbowl=Math.round(bowl)
+//     Math.round(bowl)
+  
+// if(newbowl===0){
+//   wickets1=wickets1+1
+//   alert('HOWZAT!!! thats a wicket ')
+// }
+
+// else if(newbowl===1 || newbowl===2 || newbowl===3 || newbowl===5 ){
+//   score1=score1+newbowl
+// alert(newbowl+' runs scored')
+// }
+// else if(newbowl===4 || newbowl===6){
+//   alert('WOWW!!thats a Boundry')
+// }
+//   }
+// overs=overs+1
+// // var wickets1=0
+// alert('The total Score at the end of '+overs+' Overs'+' is '+score1+' / '+wickets1)
+//   };
+
+// alert(team2+' need to '+score1+1+' runs Wins in ' +overs+' Overs')
+
+
+// var score2=0
+// var overs=0
+// var wickets2=0
+
+// alert('Lets Play')
+// for(var i=1;i<=numberofover;i++){
+//   for(var j=1;j<=6;j++){
+//     var bowl=Math.random()*7
+//     var newbowl=Math.round(bowl)
+//     Math.round(bowl)
+  
+// if(newbowl===0){
+//   wickets2=wickets2+1
+//   alert('HOWZAT!!! thats a wicket ')
+// }
+
+// else if(newbowl===1 || newbowl===2 || newbowl===3 || newbowl===4 || newbowl===5 || newbowl===6){
+//   score2=score2+newbowl
+// alert(newbowl+' runs scored ')
+// }
+// else if(newbowl===4 || newbowl===6){
+//   alert('WOWW!!thats a Boundry')
+// }
+//   }
+// overs=overs+1
+// // var wickets2=0
+// alert('The total Score at the end of '+overs+' Overs'+' is '+score2+' / '+wickets2)
+//   };
+
+// alert(toss)
+
+var rightnow=new Date()
+
+console.log(rightnow)
+
+var today=rightnow.getDate()
+console.log(today)
